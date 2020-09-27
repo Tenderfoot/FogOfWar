cedf138a505cc025a0d18a28ce6996a79e989ae4
Grant McNeil <grant.mcneil@gmail.com>
2020-09-26 23:12:51 -0400

    SOIL working

--- TwentyTwenty/src/main.cpp
+++ TwentyTwenty/src/main.cpp
@@ -3,6 +3,7 @@
 #pragma comment(lib, "SDL2main")
 #pragma comment(lib, "OpenGL32")
 #pragma comment(lib, "GLU32")
+#pragma comment(lib, "SOIL")
 
 // To be replaced with settings file
 #define USE_FULLSCREEN 1
@@ -12,13 +13,16 @@
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
-
+#include <string>
+#include "SOIL.h"
 // GLU is deprecated and I should look into removing it - only used by gluPerspective
 #include <gl/GLU.h>
 
 SDL_Window* window;
 bool done = false;
+GLuint texture;
 
+GLuint Soil_Load_Texture(std::string filename);
 
 void init_opengl()
 {
@@ -36,13 +40,15 @@ void init_opengl()
 	glMatrixMode(GL_PROJECTION);  // Select The Projection Matrix
 	glLoadIdentity();                // Reset The Projection Matrix
 	
-	// This is deprecated
+	// This is deprecated (glu.h)
 	gluPerspective(90, (float)res_width / (float)res_height, 1.0, 1000.0);
 
 	glMatrixMode(GL_MODELVIEW);  // Select The Model View Matrix
 	glLoadIdentity();    // Reset The Model View Matrix
 
 	glClearColor(0.05f, 0.05f, 0.05f, 0.5f);
+
+	texture = Soil_Load_Texture("data/skeleton.png");
 }
 
 
@@ -57,16 +63,41 @@ void handle_sdl_event()
 }
 
 
+GLuint Soil_Load_Texture(std::string filename)
+{
+	GLuint loaded_texture;
+	int flags;
+
+	flags = SOIL_FLAG_MIPMAPS;
+
+	loaded_texture = SOIL_load_OGL_texture(filename.c_str(),
+										   SOIL_LOAD_AUTO,
+										   SOIL_CREATE_NEW_ID,
+										   flags);
+
+	// Make sure texture is set to repeat on wrap
+	glBindTexture(GL_TEXTURE_2D, loaded_texture);
+
+	// make sure it doesn't wrap
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	return loaded_texture;
+}
+
+
 void draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
 	glLoadIdentity();
 
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
 	glBegin(GL_QUADS);
-		glVertex3f(-0.5f, -0.5f, -10.0f);
-		glVertex3f(0.5f, -0.5f, -10.0f);
-		glVertex3f(0.5f, 0.5f, -10.0f);
-		glVertex3f(-0.5f, 0.5f, -10.0f);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(-0.5f, -0.5f, -10.0f);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(0.5f, -0.5f, -10.0f);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(0.5f, 0.5f, -10.0f);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(-0.5f, 0.5f, -10.0f);
 	glEnd();
 
 	SDL_GL_SwapWindow(window);
