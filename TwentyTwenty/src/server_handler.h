cd05d0702a7a3c0ff883f444ec1e1ad84d0bee2c
Grant McNeil <grant.mcneil@gmail.com>
2022-01-14 01:20:12 -0500

    ITS ALIVE

--- TwentyTwenty/src/server_handler.h
+++ TwentyTwenty/src/server_handler.h
@@ -10,12 +10,6 @@ public:
     static void start_server();
 	static void run();
 
-    static IPaddress serverIP;
-    static UDPsocket udpsock;
-    static UDPpacket* recv_packet;
-    static SDLNet_SocketSet socketset;
-    static int numused;
-    static const int MAX_PACKET_SIZE = 512;
 
 protected:
 };
