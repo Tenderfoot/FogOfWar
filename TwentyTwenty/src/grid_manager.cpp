187d8533468f5e1ac5aee2d29b09d6a108ae9d79
Grant McNeil <grant.mcneil@gmail.com>
2022-01-15 11:24:54 -0500

    base case thing

--- TwentyTwenty/src/grid_manager.cpp
+++ TwentyTwenty/src/grid_manager.cpp
@@ -263,7 +263,8 @@ int GridManager::build_and_add_entity(lua_State* state)
 
 static void run_script_thread()
 {
-	lua_pcall(state, 0, LUA_MULTRET, 0);
+	//lua_pcall(state, 0, LUA_MULTRET, 0);
+	GridManager::build_and_add_entity(FOW_KNIGHT, t_vertex(10, 10, 0));
 }
 
 void GridManager::load_map(const std::string &mapname)
@@ -312,8 +313,8 @@ void GridManager::load_map(const std::string &mapname)
 	}
 
 	// execute the script
-	//script_thread = new std::thread(run_script_thread);
-	lua_pcall(state, 0, LUA_MULTRET, 0);
+	script_thread = new std::thread(run_script_thread);
+	//lua_pcall(state, 0, LUA_MULTRET, 0);
 }
 
 void GridManager::init()
