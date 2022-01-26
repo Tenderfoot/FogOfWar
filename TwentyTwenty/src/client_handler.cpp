daaaedddebf88819109f72374b55925492477f7f
Grant McNeil <grant.mcneil@gmail.com>
2022-01-26 00:08:05 -0500

    I just want the todo but theres a gold line in here

--- TwentyTwenty/src/client_handler.cpp
+++ TwentyTwenty/src/client_handler.cpp
@@ -163,7 +163,7 @@ void ClientHandler::recieve_gatherer_data(FOWGatherer* specific_character)
 	{
 		specific_character->add_to_skin("moneybag");
 	}
-	if (holding_gold == 1 && has_gold == 0)
+	if (holding_gold == 1 && has_gold == 0 && specific_character->team_id == FOWPlayer::team_id)
 	{
 		FOWPlayer::gold++;
 		specific_character->reset_skin();
