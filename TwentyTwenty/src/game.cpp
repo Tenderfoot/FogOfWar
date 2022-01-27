d6f8dae3b16168bbef09fd61788db8523a3a096e
Grant McNeil <grant.mcneil@gmail.com>
2022-01-27 03:10:52 -0500

    re-enable the minimap

--- TwentyTwenty/src/game.cpp
+++ TwentyTwenty/src/game.cpp
@@ -43,7 +43,7 @@ bool Game::init(std::string new_mapname)
 	GreenBox* new_greenbox = new GreenBox();
 	UserInterface::add_widget((UIWidget*)new_greenbox);
 	minimap = new MapWidget();
-	//UserInterface::add_widget((UIWidget*)minimap);
+	UserInterface::add_widget((UIWidget*)minimap);
 
 	new_bar = new UIProgressBar();
 	UserInterface::add_widget((UIWidget*)new_bar);
