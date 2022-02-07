ce12bcb99ff274d5270c586c3596345847799ced
Grant McNeil <grant.mcneil@gmail.com>
2022-02-07 04:57:06 -0500

    new water shader

--- TwentyTwenty/src/paintbrush.cpp
+++ TwentyTwenty/src/paintbrush.cpp
@@ -413,8 +413,8 @@ GLuint PaintBrush::Soil_Load_Texture(const std::string &filename)
 	glBindTexture(GL_TEXTURE_2D, loaded_texture);
 
 	// make sure it doesn't wrap
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
